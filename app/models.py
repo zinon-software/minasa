c64d44d367b41239ec26f5802bfa3e3a9ac2b0cf
zinon <sawh2030@gmail.com>
2021-04-20 06:53:00 +0200

    update models

--- app/models.py
+++ app/models.py
@@ -14,9 +14,16 @@ class Subject(models.Model):
 
 
 class Questions(models.Model):
+    QUESTION_TYPE = (
+        ('A', 'خيارات'),
+        ('B', 'صح وخطأ'),
+        ('C', 'وصل'),
+    )
     subject = models.ForeignKey(Subject,related_name='topics',on_delete=models.CASCADE)
     created_by = models.ForeignKey(User,related_name='topics',on_delete=models.CASCADE)
     question = models.CharField(max_length=255, null=True, blank=True)
+    shirt_size = models.CharField(max_length=1, choices=QUESTION_TYPE)
+    trueAndfalse = models.BooleanField(default=False, null=True, blank=True)
     answer_1 = models.CharField(max_length=255, null=True, blank=True)
     choice_1 = models.BooleanField(default=False, null=True, blank=True)
     answer_2 = models.CharField(max_length=255, null=True, blank=True)
