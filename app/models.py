dd39f3c3a61c9f291da568308f8e2d53f03534c1
zinon <sawh2030@gmail.com>
2021-04-22 05:17:54 +0200

    update models day 3.9

--- app/models.py
+++ app/models.py
@@ -40,7 +40,6 @@ class Questions(models.Model):
 class Students(models.Model):
     name = models.CharField(max_length=50, null=True, blank=True, unique=False)
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='subject_student')
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_dt = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
