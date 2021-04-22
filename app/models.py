029b4c309f57ada839a2516a0b6fcae9db912fb9
zinon <sawh2030@gmail.com>
2021-04-22 03:57:27 +0200

    update models 3.3day

--- app/models.py
+++ app/models.py
@@ -13,9 +13,6 @@ class Subject(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_dt = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.name
-
 
 class Questions(models.Model):
     subject = models.ForeignKey(Subject,on_delete=models.CASCADE, related_name='subject')
@@ -37,13 +34,11 @@ class Questions(models.Model):
 
 
 class Students(models.Model):
-    name = models.CharField(max_length=50, null=True, blank=True)
+    name = models.CharField(max_length=50, null=True, blank=True, unique=False)
     subject = models.ForeignKey(Subject,on_delete=models.CASCADE, related_name='subject_student')
     created_by = models.ForeignKey(User,on_delete=models.CASCADE, related_name='created_student')
     created_dt = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.name
 
 class Solutions(models.Model):
     question = models.ForeignKey(Questions,on_delete=models.CASCADE, related_name='questions')
